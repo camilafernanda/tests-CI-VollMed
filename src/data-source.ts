de37b4c70fc258561beac30a6622b7dec8fd0c3d
Camila <millanandanet14@gmail.com>
2023-08-16 16:15:44 -0300

    Test: alterando variveis de ambiente

--- src/data-source.ts
+++ src/data-source.ts
@@ -15,11 +15,11 @@ console.log("Arquivo do DataSource executado!")
 
 const AppDataSourceMysql = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST,
+  host: 'db',
   port: parseInt(process.env.DB_PORT ?? '3306'),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  username: 'root',
+  password: '12345',
+  database: 'testemed',
   synchronize: true,
   logging: false,
   entities: [Paciente, Endereco, Especialista, Avaliacoes, Clinica, Consulta, Autenticaveis, Imagem],
